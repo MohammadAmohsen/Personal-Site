2207317e8311c5e06ea12a9784142110bc3f258d
MohammadAmohsen <moeabd99@outlook.com>
2024-01-16 20:14:12 -0500

    Undid App.js changes, added navbar.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Projects from './Pages/Projects';
-import Experience from './Pages/Experience';
 
 function App() {
   return (
-    <div className="App"> 
-    <Router>
-      <Routes>
-        <Route path="/Home" element={<Home />} /> 
-        <Route path="/projects" element={<Projects />} /> 
-        <Route path="/Experience" element={<Experience />} /> 
-        </Routes>
-    </Router>
+    <div className="">
+
     </div>
   );
 }
