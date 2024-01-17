6bd4c06775c8e843af0b15c964c237e12bb243f6
MohammadAmohsen <moeabd99@outlook.com>
2024-01-16 20:35:38 -0500

    Updated nav bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='navbar'>   </div>
-  )
+    <React.Fragment>
+
+    <header id="home">
+       <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+        <a className="mobile-btn" href="youtube.com" title="Hide navigation">Hide navigation</a>
+          <ul id="nav" className="nav">
+             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+             <li><a className="smoothscroll" href="#about">About</a></li>
+           <li><a className="smoothscroll" href="#resume">Resume</a></li>/
+             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+             <li><a className="smoothscroll" href="#contact">Contact</a></li>
+          </ul>
+       </nav>
+
+       <p className="scrolldown">
+          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+       </p>
+
+    </header>
+    </React.Fragment>
+  );
 }
 
-export default Navbar
+export default Navbar;
